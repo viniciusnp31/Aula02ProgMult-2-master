40404435ce343dce92fabf79baf1651757941e5a
Vinicius <viniciusnp31@hotmail.com>
2018-03-19 09:11:10 -0300

    Aula

---
+++ app/src/main/java/br/usjt/aula02final/DialogActivity.java
@@ -0,0 +1,26 @@
+package br.usjt.aula02final;
+
+/**
+ * Vinícius Neves Pereira - 201511080 - SIN3AN-MCA1
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class DialogActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_dialog);
+    }
+
+    /**
+     * Callback method defined by the View
+     * @param v
+     */
+    public void finishDialog(View v) {
+        DialogActivity.this.finish();
+    }
+}
